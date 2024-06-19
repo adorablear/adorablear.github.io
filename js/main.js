286eba0cb090eb0909eff2f0dcc50d14cd7a6ca3
adorable_ar <52309101+adorablear@users.noreply.github.com>
2024-06-19 09:19:55 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     //메뉴 슬라이드 다운
     $('.menu-ico').click(function(){
         $('.menu').slideDown(500);
@@ -12,40 +11,6 @@ $(document).ready(function() {
     $('#scrollTopBtn').click(function() {
         $('html, body').animate({ scrollTop: 0 }, 'slow');
     });
-
-    // // .list-tit 요소 슬라이드 인
-    // $('.list-tit').addClass('visible');
-
-    // // 슬라이드 인 효과 완료 후, .box-item 요소 순차적으로 페이드 인
-    // setTimeout(function() {
-    //     $('.box-item').each(function(index) {
-    //         const item = $(this);
-    //         setTimeout(function() {
-    //             item.addClass('visible');
-    //         }, index * 300); // 순차적으로 페이드 인 효과 적용 (300ms 간격)
-    //     });
-    // }, 1000); // list-tit의 transition 시간이 1초이므로 동일하게 1000ms 설정
-
-    // myWork-sec가 화면에 보일 때
-    $(window).scroll(function() {
-        var windowHeight = $(window).height();
-        var scrollTop = $(window).scrollTop();
-        var myWorkSecTop = $('.myWork-sec').offset().top;
-        var triggerPoint =  windowHeight - myWorkSecTop + scrollTop;
-
-        if (triggerPoint > 300) {
-            $('.list-tit').addClass('visible');
-
-            setTimeout(function() {
-                $('.box-item').each(function(index) {
-                    const item = $(this);
-                    setTimeout(function() {
-                        item.addClass('visible');
-                    }, index * 300);
-                });
-            }, 1000);
-        }
-    });
 });
 
 // 맨 위로 스크롤하는 버튼 보이기/감추기
@@ -54,5 +19,27 @@ $(window).scroll(function() {
         $('#scrollTopBtn').fadeIn();
     } else {
         $('#scrollTopBtn').fadeOut();
+    }
+});
+
+
+// myWork-sec가 화면에 보일 때
+$(window).scroll(function() {
+    var windowHeight = $(window).height();
+    var scrollTop = $(window).scrollTop();
+    var myWorkSecTop = $('.myWork-sec').offset().top;
+    var triggerPoint =  windowHeight - myWorkSecTop + scrollTop;
+
+    if (triggerPoint > 300) {
+        $('.list-tit').addClass('visible');
+
+        setTimeout(function() {
+            $('.box-item').each(function(index) {
+                const item = $(this);
+                setTimeout(function() {
+                    item.addClass('visible');
+                }, index * 300);
+            });
+        }, 1000);
     }
 });
